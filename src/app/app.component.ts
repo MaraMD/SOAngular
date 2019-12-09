c531bd72349f209d98e0d9f7d3809e0bb4f34042
manueldaniel96 <manueldaniel96@gmail.com>
2019-12-09 08:14:19 -0600

    :'c

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,10 +19,10 @@ export class AppComponent implements OnInit{
   ngOnInit(){
     this.identity = this.apiService.getToken()
     console.log(this.identity);
-    // if(this.identity == null){
+      if(this.identity == null){
         this.router.navigate(['/login']);
-  //   }else
-  //     this.router.navigate(['/home']);
+      }else
+        this.router.navigate(['/home']);
   //     //this.router.navigate(['/busqueda']);
    }
 }
